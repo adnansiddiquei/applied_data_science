cd04ac8bc2d5de288b5513a3552d36ad2c4694f2
adnansiddiquei <adnanshirik@gmail.com>
2023-12-14 15:09:01 +0000

    Completed code for q1e.

--- src/dataset_a/scripts/__init__.py
+++ src/dataset_a/scripts/__init__.py
@@ -2,3 +2,4 @@ from ._q1a import q1a
 from ._q1b import q1b
 from ._q1c import q1c
 from ._q1d import q1d
+from ._q1e import q1e
---
+++ src/dataset_a/scripts/_q1e.py
@@ -0,0 +1,62 @@
+from .q1utils import kmeans_on_dataset_a
+from src.utils import format_axes, load_dataset
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+import pandas as pd
+import os
+
+
+def q1e():
+    _contingency_table, tbl, kmeans_1_all, kmeans_2_all = kmeans_on_dataset_a(
+        n_clusters=3, random_state=3438
+    )
+
+    data = load_dataset(
+        'A_NoiseAdded.csv',
+        drop_columns=['Unnamed: 0', 'classification'],
+        standardise=True,
+    )
+
+    pca = PCA(n_components=2)
+    pca.fit(data)
+
+    # Compute the scores on each observation
+    z = pd.DataFrame(pca.transform(data))
+
+    # Add the classifications to the DataFrame
+    z['classification_1'] = kmeans_1_all
+    z['classification_2'] = kmeans_2_all
+
+    # Plot the PCA and the classifications together
+    color_map = {0: 'blue', 1: 'red', 2: 'green'}
+    fig, ax = plt.subplots(figsize=(16, 6), nrows=1, ncols=2)
+
+    for classification, color in color_map.items():
+        subset = z[z['classification_1'] == classification]
+        ax[0].scatter(
+            subset[0],
+            subset[1],
+            c=color,
+            label=f'Cluster {classification + 1}',
+            alpha=0.5,
+        )
+
+    for classification, color in color_map.items():
+        subset = z[z['classification_2'] == classification]
+        ax[1].scatter(
+            subset[0],
+            subset[1],
+            c=color,
+            label=f'Cluster {classification + 1}',
+            alpha=0.5,
+        )
+
+    # Format the plots
+    ax[0].legend()
+    ax[1].legend()
+
+    format_axes(ax[0])
+    format_axes(ax[1])
+
+    cwd = os.path.dirname(os.path.realpath(__file__))
+    plt.savefig(os.path.join(cwd, '../outputs/q1e.png'), bbox_inches='tight')
