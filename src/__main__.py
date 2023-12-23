b357f1e51c896922895c368cdc50141926eae3cb
adnansiddiquei <adnanshirik@gmail.com>
2023-12-23 13:13:54 +0000

    Created a run all script.

---
+++ src/__main__.py
@@ -0,0 +1,12 @@
+from src.q1 import q1_run_all
+from src.q2 import q2_run_all
+from src.q3 import q3_run_all
+from src.q4 import q4_run_all
+from src.q5 import q5_run_all
+
+if __name__ == '__main__':
+    q1_run_all()
+    q2_run_all()
+    q3_run_all()
+    q4_run_all()
+    q5_run_all()
